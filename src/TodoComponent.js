fdf03178c3e593cb2c72813a90cc415622359938
Vlad Tsesarsky <v.tsesarsky@gmail.com>
2018-12-09 11:31:46 +0300

    add task

--- src/TodoComponent.js
+++ src/TodoComponent.js
@@ -5,19 +5,32 @@ class TodoComponent extends React.Component {
     constructor() {
         super();
         this.state = {
-            data: [
-                {
-                    id: 1,
-                    title: 'Выучить React.js',
-                    status: false
-                },
-                {
-                    id: 2,
-                    title: 'Выучить React.js',
-                    status: false
-                }
-            ]
+            data: [],
+            current: ''
         }
+    }
+    readValue = (e) => {
+        this.setState({
+            current: e.target.value
+        });
+    }
+    addTask = (e) => {
+        e.preventDefault();
+        const current = {
+            id: this.state.data.length !== 0 ? this.state.data[this.state.data.length - 1].id + 1 : 0,
+            title: this.state.current,
+            status: false
+        }
+        const dublicated = this.state.data.some((element) => {
+            return element.title === current.title;
+        })
+        if (dublicated) {
+            return;
+        }
+        this.setState({
+            data: this.state.data.concat(current),
+            current: ''
+        });
     }
     render() {
         const tasks = this.state.data.map((item) => {
@@ -29,8 +42,8 @@ class TodoComponent extends React.Component {
         return (
             <div>
                 <h1>Менеджер задач</h1>
-                <input type="text" placeholder="Добавьте новую задачу"/>
-                <input type="submit" value="Добавить" />
+                <input type="text" value={this.state.current} onChange={this.readValue} placeholder="Добавьте новую задачу"/>
+                <input type="submit" value="Добавить" onClick={this.addTask}/>
                 <ul>{tasks}</ul>
             </div>
         );
